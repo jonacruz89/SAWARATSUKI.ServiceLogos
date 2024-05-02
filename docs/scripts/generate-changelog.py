e69bcc7e42466c80c165f64701d9931b31efbe21
student_2333 <lgc2333@126.com>
2024-05-02 22:18:26 +0800

    refactor

--- docs/scripts/generate-changelog.py
+++ docs/scripts/generate-changelog.py
@@ -12,6 +12,7 @@ from typing import cast
 from uuid import uuid4
 
 from githubkit import GitHub
+from githubkit.versions.v2022_11_28.models import Release
 
 ENC = locale.getpreferredencoding()
 
@@ -34,10 +35,14 @@ gh_repo = cast(
 )
 gh_output_path = os.environ["GITHUB_OUTPUT"]
 
+tag_suffix = f"_{gh_ref_name}"
+
 
 def set_output(key: str, value: str):
     delimiter = f"ghadelimiter_{uuid4()}"
-    string = f"{key}<<{delimiter}{os.linesep}{value}{os.linesep}{delimiter}{os.linesep}"
+    if delimiter in value:
+        raise ValueError(f"Delimiter {delimiter} found in value {value!r}")
+    string = f"{key}<<{delimiter}\n{value}\n{delimiter}\n".replace("\n", os.linesep)
     with open(gh_output_path, "a", encoding="u8") as f:  # noqa: PTH123
         f.write(string)
 
@@ -66,27 +71,43 @@ def run(cmd: list[str]) -> str:
     return stdout.strip()
 
 
-def main():
-    tag_suffix = f"_{gh_ref_name}"
-
-    print(f"Getting latest release on branch {gh_ref_name} by tag name")
+def find_parent_release() -> Release | None:
     # check latest release first
-    has_parent_release = True
     release = gh.rest.repos.get_latest_release(*gh_repo).parsed_data
     if release.tag_name.endswith(tag_suffix):
-        parent_sha = release.target_commitish
-    else:
-        for release in gh.rest.repos.list_releases(*gh_repo).parsed_data:
-            if release.tag_name.endswith(tag_suffix):
-                parent_sha = release.target_commitish
-                break
-        else:
-            print("No release satisfied, will compare this commit with parent commit.")
-            has_parent_release = False
-            parent_sha = run(["git", "rev-parse", f"{gh_sha}^"])
+        return release
 
-    print(f"Comparing {parent_sha}..{gh_sha}")
+    for page in range(1, 4):  # find 3 pages max
+        release_list = gh.rest.repos.list_releases(*gh_repo, page=page).parsed_data
+        if not release_list:
+            break
+
+        release = next(
+            (
+                release
+                for release in release_list
+                if release.tag_name.endswith(tag_suffix)
+            ),
+            None,
+        )
+        if release:
+            return release
+
+    return None
+
+
+def main():
+    print(f"Getting latest release on branch {gh_ref_name} by tag name")
+    parent_release = find_parent_release()
+    if parent_release:
+        print(f"Latest release found: {parent_release.tag_name}")
+        parent_sha = parent_release.target_commitish
+    else:
+        print("No release satisfied, will compare this commit with parent commit.")
+        parent_sha = run(["git", "rev-parse", f"{gh_sha}^"])
+
     order = list(NAME.keys())
+    print(f"Comparing {parent_sha}..{gh_sha}")
     try:
         diff = run(
             [
@@ -99,35 +120,36 @@ def main():
             ],
         )
     except RuntimeError as e:
+        print("Failed to get difference between parent release.")
         print(e)
-        # changelog = "Failed to get difference between parent release."
         exit(1)
-    else:
-        diff_spiltted = sorted(
-            [
-                [splitted[0][0], *splitted[1:]]
-                for line in diff.splitlines()
-                if (
-                    line
-                    and len(splitted := line.split("\t")) > 1
-                    and splitted[1].endswith(".png")
-                )
-            ],
-            key=lambda x: order.index(x[0]),
-        )
-        if not diff_spiltted:
-            print("No image changes detected.")
-            if has_parent_release:
-                return
-            print("Should create initial release.")
-        changelog = "\n".join(
-            [
-                f"- **{NAME.get(status)}**: {' -> '.join(f'`{x}`' for x in rest)}"
-                for status, *rest in diff_spiltted
-            ],
-        )
 
-    if not has_parent_release:
+    diff_splitted = sorted(
+        (
+            (splitted[0][0], *splitted[1:])
+            for line in diff.splitlines()
+            if (
+                line
+                and len(splitted := line.split("\t", maxsplit=2)) > 1
+                and splitted[1].endswith(".png")
+            )
+        ),
+        key=lambda x: order.index(x[0]),
+    )
+    if not diff_splitted:
+        print("No image changes detected.")
+        if parent_release:
+            print("Parent release found but no changes detected, will exit.")
+            return
+
+    changelog = "\n".join(
+        [
+            f"- **{NAME.get(status)}**: {' -> '.join(f'`{x}`' for x in rest)}"
+            for status, *rest in diff_splitted
+        ],
+    )
+    if not parent_release:
+        print("No parent release found, should create initial release.")
         changelog = f"Initial release.\n\n{changelog}"
     else:
         parent_sha_short = parent_sha[:7]
